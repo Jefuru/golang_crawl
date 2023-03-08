42dc2c78cb6e083049c59e921737389f2844e982
JeffLiu <lcf4020708@gmail.com>
2023-03-08 22:58:10 +0800

    compound interest

---
+++ compound_interest.go
@@ -0,0 +1,21 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"math"
+// )
+
+// func main(){
+// 	var principalAmount, interestRate, years float64
+
+// 	fmt.Println("Plz enter principal amount:")
+// 	fmt.Scanln(&principalAmount)
+
+// 	fmt.Println("Now enter interest rate:")
+// 	fmt.Scanln(&interestRate)
+
+// 	fmt.Println("Last step enter year:")
+// 	fmt.Scanln(&years)
+
+// 	fmt.Printf("Your estimated compoundInterset is: %f", principalAmount*(math.Pow((1 + interestRate / 100), years) - 1))
+// }
